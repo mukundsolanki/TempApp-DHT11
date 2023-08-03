fb0f45e3b4e8dcd11e53a333ed4aca12b9ef1dda
Mukund Solanki <mukundsolanki665@gmail.com>
2023-08-03 22:10:58 +0530

    Updating the Home page UI

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,16 @@ class SensorApp extends StatefulWidget {
 class _SensorAppState extends State<SensorApp> {
   double temperature = 0;
   double humidity = 0;
+  String statusCode = "Null";
 
   void fetchSensorData() async {
     final response = await http.get(Uri.parse('http://192.168.29.80:5000/sensor_data'));
+    setState(() {
+      statusCode = response.statusCode == 200 ? response.statusCode.toString() : 'Null'; 
+    });
     if (response.statusCode == 200) {
+      final data = json.decode(response.body);
       setState(() {
-        final data = json.decode(response.body);
         temperature = data['temperature'];
         humidity = data['humidity'];
       });
@@ -36,19 +40,123 @@ class _SensorAppState extends State<SensorApp> {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text('DHT11 Sensor Data'),
+          title: const Text('DHT11 Sensor Data'),
         ),
-        body: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text('Temperature: $temperature°C'),
-              Text('Humidity: $humidity%'),
-              ElevatedButton(
-                onPressed: fetchSensorData,
-                child: Text('Refresh'),
+        body: Container(
+          decoration: BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Colors.blue[400]!, Colors.blue[900]!],
+            ),
+          ),
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: <Widget>[
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Container(
+                      width: 200,
+                      height: 120,
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.thermostat_auto, size: 40, color: Colors.blue[900]),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Temperature',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '$temperature°C',
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 16),
+                  Card(
+                    elevation: 4,
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(16.0),
+                    ),
+                    child: Container(
+                      width: 200,
+                      height: 120,
+                      padding: const EdgeInsets.all(16.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                        children: [
+                          Icon(Icons.water, size: 40, color: Colors.blue[900]),
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              const Text(
+                                'Humidity',
+                                style: TextStyle(
+                                  fontSize: 18,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                              Text(
+                                '$humidity%',
+                                style: const TextStyle(
+                                  fontSize: 24,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  const SizedBox(height: 24),
+                  ElevatedButton(
+                    onPressed: fetchSensorData,
+                    style: ElevatedButton.styleFrom(
+                      backgroundColor: Colors.transparent,
+                      foregroundColor: Colors.white,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(8.0),
+                      ),
+                      shadowColor: Colors.transparent, // Add shadow color
+                      elevation: 4, // Add elevation for shadow effect
+                    ),
+                    child: const Icon(Icons.refresh, size: 40),
+                  ),
+                  const SizedBox(height: 8),
+                  Text(
+                    'Status Code: $statusCode',
+                    style: const TextStyle(
+                      fontSize: 16,
+                      color: Colors.white,
+                    ),
+                  ),
+                ],
               ),
-            ],
+            ),
           ),
         ),
       ),
